4691239e11ffc36da5f59359b5808d4ea73c233a
Ashfak Uddin <ashmanpapet@gmail.com>
2025-03-04 20:08:10 -0500

    Edited function names

--- script.js
+++ script.js
@@ -2,24 +2,34 @@
 let numRows = 0;
 let numCols = 0;
 let colorSelected; 
+/* 
+<tr>
+    <td onclick="alert('Clicked a table cell');"></td>
+    <td onclick="alert('Clicked a table cell');"></td>
+    <td onclick="alert('Clicked a table cell');"></td>
+</tr> 
+*/
+function getGrid() {
+    return document.getElementById("grid");
+}
 
 // Add a row
-function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+function addRow() {
+    console.log(getGrid());
 }
 
 // Add a column
-function addC() {
+function addColumn() {
     alert("Clicked Add Col"); // Replace this line with your code.
 }
 
 // Remove a row
-function removeR() {
+function removeRow() {
     alert("Clicked Remove Row"); // Replace this line with your code.
 }
 
 // Remove a column
-function removeC() {
+function removeColumn() {
     alert("Clicked Remove Col"); // Replace this line with your code.
 }
 
@@ -30,16 +40,16 @@ function selectColor(){
 }
 
 // Fill all uncolored cells
-function fillU(){
+function fillUncoloredCells(){
     alert("Clicked Fill All Uncolored"); // Replace this line with your code.
 }
 
 // Fill all cells
-function fillAll(){
+function fillAllCells(){
     alert("Clicked Fill All"); // Replace this line with your code.
 }
 
 // Clear all cells
-function clearAll(){
+function clearAllCells(){
     alert("Clicked Clear All"); // Replace this line with your code.
 }
